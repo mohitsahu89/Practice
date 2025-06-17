53602909bf56e3f3090f4333ef61597e30a6dd07
mohitsahu89 <mohitsahu89@gmail.com>
2025-06-18 00:31:13 +0530

    Practice

--- practice.py
+++ practice.py
@@ -2,6 +2,15 @@ from collections import Counter
 
 items = [1, 2, 2, 3, 4, 4, 4, 5, 1]
 counter = Counter(items)
-print(counter)
 first_unique = next(iter({key: value for key, value in counter.items() if value == 1}))
 print(first_unique)
+
+
+longest_string = "Neighbourhood"
+read = ""
+for i in longest_string:
+    if i not in read:
+        read += i  
+    else:
+        break
+print(read)
